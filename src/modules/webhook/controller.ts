48efaf2da2584faf9751c77a9bc22d597eb49ffb
郑家豪 <beatboxzjh@163.com>
2025-12-16 18:48:53 +0800

    feat(webhook): 增加查询任务接口

--- src/modules/webhook/controller.ts
+++ src/modules/webhook/controller.ts
@@ -1,4 +1,7 @@
 import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model, Types } from 'mongoose';
+import { Mission } from '../database/schemas/mission';
 import { NotificationService } from '../notification/service';
 import { MergeRequestDto } from './dto';
 import { WebhookService } from './service';
@@ -8,6 +11,7 @@ export class WebhookController {
   constructor(
     private notificationService: NotificationService,
     private webhookService: WebhookService,
+    @InjectModel(Mission.name) private missionModel: Model<Mission>,
   ) {}
 
   @Get('/triggerNotification')
@@ -18,5 +22,19 @@ export class WebhookController {
   @Post('gitlab/:teamId')
   async gitlab(@Body() body: MergeRequestDto, @Param('teamId') teamId: string) {
     await this.webhookService.gitlab(body, teamId);
+  }
+
+  @Get('getMissionList')
+  async getMissionList(@Query('memberId') memberId: string) {
+    const missionList = await this.missionModel
+      .find({
+        $or: [
+          { sourceMemberId: new Types.ObjectId(memberId) },
+          { targetMemberId: new Types.ObjectId(memberId) },
+        ],
+      })
+      .lean();
+
+    return missionList;
   }
 }
